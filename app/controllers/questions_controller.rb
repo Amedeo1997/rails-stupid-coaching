8178e4932614c6179804323531619af935ac258c
ahmed salhi <sirjaizz@gmail.com>
2023-11-13 13:01:56 +0100

    stupid coaching no design

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def new
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_response(@question)
+  end
+
+  def coach_response(question)
+    if question.downcase == 'i am going to work'
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
